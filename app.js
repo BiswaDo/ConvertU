753c1b90f6bdc3995f1c28964ee030d16dd83858
BiswaDo <biswadipsaha503@gmail.com>
2024-02-27 13:56:55 +0530

    few changes

--- app.js
+++ app.js
@@ -56,9 +56,12 @@ app.post("/download-mp3", async function (req, res) {
 
             response.on("end", () => {
                 const fetchResponce = JSON.parse(data);
+                console.log(fetchResponce)
                 res.write("<h1>Song name :"+fetchResponce.title+"</h1>");
                 res.write("<h2>Click here to download:- <a href='"+fetchResponce.link+"'>Download</a></h2>");
                 res.send();
+                // res.render("message", { songName: fetchResponce.title, songLink: fetchResponce.link });
+
             });
         });
 
